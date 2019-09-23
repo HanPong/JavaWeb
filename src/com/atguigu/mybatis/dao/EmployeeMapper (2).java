96b172c2950dc8c5f8e14a6564f198893305c71e
HanPong <1849062283@qq.com>
2019-09-23 22:35:32 +0800

    second commit

--- src/com/atguigu/mybatis/dao/EmployeeMapper (2).java
+++
@@ -1,7 +0,0 @@
-package com.atguigu.mybatis.dao;
-import com.atguigu.mybatis.bean.Employee;
-//此接口就是用于查询并封装成Employee对象的
-public interface EmployeeMapper {
-	public Employee getEmpById(Integer id);
-
-}
